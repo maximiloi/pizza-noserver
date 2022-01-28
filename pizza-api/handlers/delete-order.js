70e2915bb444a009f64ee0b6f3cb845cd608f8ce
maximiloi <maximiloi@gmail.com>
2022-01-29 00:41:37 +0300

    ref deleteOrder added work with DynamoDB

--- pizza-api/handlers/delete-order.js
+++ pizza-api/handlers/delete-order.js
@@ -1,9 +1,23 @@
-function deleteOrder(id) {
-  if (!id) {
-    // если в нет id заказа, выдать ошибку
-    throw new Error("Заказа №${id} не существует");
-  }
-  return {};
+"use strict";
+
+const AWS = require("aws-sdk");
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+function deleteOrder(orderId) {
+  return docClient
+    .delete({
+      TableName: "pizza-orders",
+      Key: { orderId: orderId },
+    })
+    .promise()
+    .then((result) => {
+      console.log("Заказ удален", result);
+      return result;
+    })
+    .catch((deleteError) => {
+      console.log("Заказ не удален", deleteError);
+      throw deleteError;
+    });
 }
 
 module.exports = deleteOrder;
