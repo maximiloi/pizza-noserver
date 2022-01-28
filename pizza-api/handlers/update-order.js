0177a81fbc085730c5983a6b4687b6189e1c4e9d
maximiloi <maximiloi@gmail.com>
2022-01-29 00:35:39 +0300

    ref updateOrder added work with DynamoDB

--- pizza-api/handlers/update-order.js
+++ pizza-api/handlers/update-order.js
@@ -4,12 +4,29 @@ const AWS = require("aws-sdk");
 const docClient = new AWS.DynamoDB.DocumentClient();
 
 function updateOrder(orderId, options) {
-  if (!options || !options.pizza || !options.address) {
+  if (!options || !options.pizza || !options.address)
     throw new Error("Проверте номера заказа или укажите что сделать с заказом");
-  }
-  return {
-    message: "Заказ № ${id}, изменен",
-  };
+
+  return docClient
+    .update({
+      TableName: "pizza-orders",
+      Key: { orderId: orderId },
+      UpdateExpression: "set pizza = :p, addres = :a",
+      ExpressionAttributeValues: {
+        ":p": options.pizza,
+        ":a": options.address,
+      },
+      ReturnValues: "ALL_NEW",
+    })
+    .promise()
+    .then((result) => {
+      console.log("Заказ обновлен", result);
+      return result.Attributes;
+    })
+    .catch((updateError) => {
+      console.log("С заказом что то не так", updateError);
+      throw updateError;
+    });
 }
 
 module.exports = updateOrder;
